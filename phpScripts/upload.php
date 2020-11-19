e09fb1ddfcde37c3b8477887c1f740ab41507615
mn800 <38965136+mn800@users.noreply.github.com>
2020-11-19 18:21:08 -0500

    Added Anthony's Encoder
    Added Anthony's Encoder as well as editing the upload php script

--- phpScripts/upload.php
+++ phpScripts/upload.php
@@ -4,14 +4,21 @@ require_once ('connectvars.php');
 $dbc = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 $params = $_POST;
 
-// Image will be uploaded encoded in Base64
-$image = base64_decode(params['p5']);
+// Decodes the Base64 String of the image
+$imgbin = base64_decode($params['p4']);
 
-// Still have to look into how to insert the image into the table
-$list = sprintf("INSERT INTO %s VALUES (%s,%s,%s,%s);", $params['p1'], $params['p2'],$param['p3'], $param['p4'], $image);
+$null = null;
+$sql = "INSERT INTO Pictures VALUES (?,?,?,?)";
+$stmt = $dbc->prepare($sql);
 
-$result = $dbc->query($list);
 
-$result -> free_result();
+$stmt->bind_param("sssb", $params['p1'], $params['p2'], $params['p3'], $null);
+
+// Sends the binary image
+$stmt->send_long_data(3, $imgbin);
+
+$stmt->execute();
+
+$stmt->close();
 $dbc->close();
 ?>
---
+++ src/connectDB/Base64Encoder.java
@@ -0,0 +1,31 @@
+package connectDB;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Base64;
+
+public class Base64Encoder {
+	public static void main(String[] args) {
+	    File f =  new File("C:/Users/Desktop/name.jpg"); //Don't know how to save to database
+	    String encodstring = encodeFileToBase64Binary(f);
+	    System.out.println(encodstring);
+    }
+	
+	private static String encodeFileToBase64Binary(File file){
+	    String encodedfile = null;
+	    try {
+	    	FileInputStream fileInputStreamReader = new FileInputStream(file);
+	    	byte[] bytes = new byte[(int)file.length()];
+	    	fileInputStreamReader.read(bytes);
+	    	encodedfile = new String(Base64.encodeBase64(bytes), "UTF-8");
+	    }
+	    catch (FileNotFoundException e) {
+	    	e.printStackTrace();
+	    }
+	    catch (IOException e) {
+	    	e.printStackTrace();
+    	}
+    	return encodedfile;
+	    }
+}
